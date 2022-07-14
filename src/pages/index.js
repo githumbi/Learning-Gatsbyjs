88cf0d59302aabdf7a89ab36bd750a18f9a2dda6
githumbi <githumbi74@gmail.com>
2022-07-14 13:36:30 +0300

    css modules

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,15 @@
 import React from "react"
 import { Link } from "gatsby"
 import Layout from "../components/Layout"
+// import {page} from '../examples/home.module.css'
+import * as homestyle from '../examples/home.module.css' //saves time 
 
 const index = () => {
   return (
     <Layout>
-      <div className="home-header">
+      <div className={homestyle.page}>
         <h1>Home page</h1>
-        <h1 style={{ color: "red" }}>Home page</h1>
+        <h1>Home page</h1>
         <div>
           <Link to="/about">about</Link>
           <Link to="/company/company">company</Link>
